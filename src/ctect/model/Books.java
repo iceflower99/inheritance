67ab60fdafe2938cf93bce6e2ec94dfe2d33c8f6
iceflower99 <mhal4767@740s-pl-1280.local>
2016-03-30 07:37:05 -0600

    made a new project called inheritance

---
+++ src/ctect/model/Books.java
@@ -0,0 +1,30 @@
+package ctect.model;
+
+/**
+ * @author madeleine hales	
+ *@ version 0.x Mar 28,2016
+ */
+
+public abstract class Books
+{
+   //Three data members
+	private String author;
+	private String title;
+	private int pages;
+	
+	public String author()
+	{
+		return author;
+	}
+	
+	public String title()
+	{
+		return title;
+	}
+	
+	public int pages()
+	{
+		return pages;
+	}
+	
+}
---
+++ src/ctect/model/PercyJackson.java
@@ -0,0 +1,8 @@
+package ctect.model;
+
+import books;
+
+public class PercyJackson extends books
+{
+
+}
